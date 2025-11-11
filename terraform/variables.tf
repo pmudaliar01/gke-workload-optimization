74a6520baf92e566e3b2c343edebb23895f67009
pmudaliar01 <92157507+pmudaliar01@users.noreply.github.com>
2025-11-11 23:45:08 +0530

    Refactor variable declarations in variables.tf
    Reformatted variable declarations for better readability.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,18 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "us-central1" }
-variable "zone"       { type = string  default = "us-central1-a" }
-variable "cluster_name" { type = string default = "test-cluster" }
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "test-cluster"
+}
